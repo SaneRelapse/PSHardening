d09a9f7be0dbede8aa9c81379af07ed186a61447
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-01 05:52:13 -0900

    Add files via upload
    Mitigate Uptime calculation
    Mitigate Host identification using clock skew

---
+++ Disable TCP Timestamps.ps1
@@ -0,0 +1,4 @@
+﻿netsh int tcp set global timestamps=disabled
+
+
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -Name "Tcp1323Opts" -Value 0 -PropertyType DWORD -Force
