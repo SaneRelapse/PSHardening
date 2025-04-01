7d7748a9a16fbbc6617c2cc1708a9b18ec891fa3
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-01 10:42:43 -0900

    Add files via upload
    Blocks the default TCP, UDP from Wifi pineapple

---
+++ Block Wifi Pineapple.ps1
@@ -0,0 +1,5 @@
+﻿#Wifi Pineapple Remote
+New-NetFirewallRule -DisplayName "Block Outbound Port 1471 Wifi Pineapple" -Direction Outbound -RemotePort 1471 -Protocol TCP -Action Block
+New-NetFirewallRule -DisplayName "Block Inbound Port 1471 Wifi Pineapple" -Direction Inbound -RemotePort 1471 -Protocol TCP -Action Block
+New-NetFirewallRule -DisplayName "Block Outbound Port 1471 Wifi Pineapple" -Direction Outbound -RemotePort 1471 -Protocol UDP -Action Block
+New-NetFirewallRule -DisplayName "Block Inbound Port 1471 Wifi Pineapple" -Direction Inbound -RemotePort 1471 -Protocol UDP -Action Block
