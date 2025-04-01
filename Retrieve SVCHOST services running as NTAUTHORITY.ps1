9626bbcab133251945594b4e35c74b8d62dbf708
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-01 11:17:36 -0900

    Add files via upload

---
+++ Retrieve SVCHOST services running as NTAUTHORITY.ps1
@@ -0,0 +1,3 @@
+﻿Get-WmiObject -Class Win32_Service | 
+    Where-Object {$_.ProcessID -in (Get-Process -IncludeUserName -Name svchost | Where-Object {$_.UserName -eq 'NT AUTHORITY\SYSTEM'}).Id} | 
+    Select-Object Name, DisplayName, State, ProcessID
