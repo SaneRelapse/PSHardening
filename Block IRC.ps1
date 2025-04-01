12c612cb910f23a00ae1bcf0fb54fda9c6359add
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-01 10:55:45 -0900

    Add files via upload

---
+++ Block IRC.ps1
@@ -0,0 +1,2 @@
+﻿# Block IRC-related ports: 194, 529, 6660-6669, 6697, and 7000
+New-NetFirewallRule -DisplayName "Block IRC Ports" -Direction Inbound -Protocol TCP -LocalPort 194,529,6660-6669,6697,7000 -Action Block -Description "Blocking IRC-related ports: 194, 529, 6660-6669, 6697, and 7000"
