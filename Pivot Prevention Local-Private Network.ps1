e771ad7d441221efd5cd68310ac3d3202860b523
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-01 06:34:38 -0900

    Update Pivot Prevention Local-Private Network.ps1

--- Pivot Prevention Local-Private Network.ps1
+++ Pivot Prevention Local-Private Network.ps1
@@ -1,3 +1,7 @@
+# Pivoting is a hacking technique where an attacker gains initial access to a compromised system and then moves laterally through a network to access other systems.
+# This method is commonly used in post-exploitation scenarios, allowing attackers to bypass security controls and reach deeper into a network.
+# This Script hopes to prevent these types of attacks.
+
 ﻿# Block all inbound traffic (all protocols)
 New-NetFirewallRule -DisplayName "Block All Inbound Protocols" -Direction Inbound -Action Block -Protocol Any -Profile PRIVATE
 
