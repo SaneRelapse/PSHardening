47467c5bebc5a958b1cea63f486d186f7c4e8e9a
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-01 10:45:41 -0900

    Add files via upload

---
+++ Block HPING3 DDOS-DOS.ps1
@@ -0,0 +1,5 @@
+﻿# Block Hping3 DDOS 
+New-NetFirewallRule -DisplayName "Block Outbound Port 0 NULL TCP HPING3 DDOS" -Direction Outbound -LocalPort 0 -Protocol TCP -Action Block
+New-NetFirewallRule -DisplayName "Block Inbound Port 0 NULL TCP HPING3 DDOS" -Direction Inbound -LocalPort 0 -Protocol TCP -Action Block
+New-NetFirewallRule -DisplayName "Block Outbound Port 0 NULL TCP HPING3 DDOS" -Direction Outbound -LocalPort 0 -Protocol UDP -Action Block
+New-NetFirewallRule -DisplayName "Block Inbound Port 0 NULL TCP HPING3 DDOS" -Direction Inbound -LocalPort 0 -Protocol UDP -Action Block
