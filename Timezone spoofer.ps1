7f3c54271465427a5c4e80627bc409267d5ca27e
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-01 05:08:39 -0900

    Add files via upload
    This PowerShell script is designed to continuously spoof your system's time zone by cycling through a comprehensive list of available Windows time zone IDs. The script operates in an infinite loop, pausing for five seconds between each change. By doing so, it systematically sets the system time zone to various regional identifiers—from "Dateline Standard Time" to "Line Islands Standard Time."

---
+++ Timezone spoofer.ps1
@@ -0,0 +1,285 @@
+﻿for (;;)
+{
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Dateline Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "UTC-11"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Aleutian Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Hawaiian Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Marquesas Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Alaskan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "UTC-09"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Pacific Standard Time (Mexico)"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "UTC-08"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Pacific Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "US Mountain Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Mountain Standard Time (Mexico)"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Mountain Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Yukon Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Central America Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Central Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Easter Island Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Central Standard Time (Mexico)"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Canada Central Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "SA Pacific Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Eastern Standard Time (Mexico)"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Eastern Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Haiti Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Cuba Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "US Eastern Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Turks And Caicos Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Paraguay Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Atlantic Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Venezuela Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Central Brazilian Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "SA Western Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Pacific SA Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Newfoundland Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Tocantins Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "E. South America Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "SA Eastern Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Argentina Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Greenland Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Montevideo Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Magallanes Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Saint Pierre Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Bahia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "UTC-02"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Mid-Atlantic Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Azores Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Cape Verde Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "UTC"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "GMT Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Greenwich Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Sao Tome Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Morocco Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "W. Europe Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Central Europe Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Romance Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Central European Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "W. Central Africa Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Jordan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "GTB Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Middle East Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Egypt Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "E. Europe Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Syria Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "West Bank Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "South Africa Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "FLE Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Israel Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "South Sudan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Kaliningrad Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Sudan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Libya Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Namibia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Arabic Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Turkey Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Arab Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Belarus Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Russian Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "E. Africa Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Volgograd Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Iran Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Arabian Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Astrakhan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Azerbaijan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Russia Time Zone 3"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Mauritius Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Saratov Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Georgian Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Caucasus Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Afghanistan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "West Asia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Ekaterinburg Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Pakistan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Qyzylorda Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "India Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Sri Lanka Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Nepal Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Central Asia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Bangladesh Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Omsk Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Myanmar Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "SE Asia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Altai Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "W. Mongolia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "North Asia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "N. Central Asia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Tomsk Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "China Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "North Asia East Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Singapore Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "W. Australia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Taipei Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Ulaanbaatar Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Aus Central W. Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Transbaikal Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Tokyo Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "North Korea Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Korea Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Yakutsk Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Cen. Australia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "AUS Central Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "E. Australia Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "AUS Eastern Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "West Pacific Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Tasmania Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Vladivostok Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Lord Howe Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Bougainville Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Russia Time Zone 10"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Magadan Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Norfolk Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Sakhalin Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Central Pacific Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Russia Time Zone 11"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "New Zealand Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "UTC+12"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Fiji Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Kamchatka Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Chatham Islands Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "UTC+13"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Tonga Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Samoa Standard Time"
+Start-Sleep -Seconds 5
+Set-TimeZone -Id "Line Islands Standard Time"
+}
