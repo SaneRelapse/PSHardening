6ed154ed58dac559c8ac592502b8a213299758a9
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-01 06:03:34 -0900

    Add files via upload
    Block ICMP Timestamp Requests

---
+++ Block ICMP Timestamp Requests.ps1
@@ -0,0 +1,5 @@
+﻿# Block ICMP Timestamp Request (Type 13)
+New-NetFirewallRule -DisplayName "Block ICMP Timestamp Request" -Protocol ICMPv4 -IcmpType 13 -Direction Inbound -Action Block
+
+# Block ICMP Timestamp Reply (Type 14)
+New-NetFirewallRule -DisplayName "Block ICMP Timestamp Reply" -Protocol ICMPv4 -IcmpType 14 -Direction Inbound -Action Block
