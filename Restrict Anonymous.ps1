f51e3487c4b136cc18458db86b6096548d784c73
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-02 05:36:48 -0900

    Add files via upload

---
+++ Restrict Anonymous.ps1
@@ -0,0 +1,7 @@
+﻿# What it does: Configures Windows to disallow anonymous users from enumerating system accounts and resources.
+
+# Why it's important: It minimizes security risks by preventing unauthorized access via null sessions.
+
+# Overall impact: Enhances system security, though it may require adjustments for legacy applications relying on anonymous access.
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa' -Name RestrictAnonymous -Value 2
