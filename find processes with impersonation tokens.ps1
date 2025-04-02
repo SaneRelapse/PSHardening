691cf554feef7ce15e961f0941571066d48b7805
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-02 05:55:39 -0900

    Add files via upload

---
+++ find processes with impersonation tokens.ps1
@@ -0,0 +1,11 @@
+﻿Get-WmiObject Win32_Process | ForEach-Object {
+    $proc = $_
+    $owner = $proc.GetOwner()
+    $token = $proc.GetOwnerSid()
+    [PSCustomObject]@{
+        ProcessName = $proc.Name
+        ProcessID = $proc.ProcessId
+        UserName = "$($owner.Domain)\$($owner.User)"
+        UserSID = $token
+    }
+} | Format-Table -AutoSize
