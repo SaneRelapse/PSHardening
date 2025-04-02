7559a6be790dcfa3be1621aafe016cb8a47e0c06
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-02 07:02:23 -0900

    Update Block SMS via Phone Link (Your Phone App).ps1

--- Block SMS via Phone Link (Your Phone App).ps1
+++ Block SMS via Phone Link (Your Phone App).ps1
@@ -1,1 +1,6 @@
-﻿reg add "HKCU\Software\Microsoft\YourPhone" /v "SmsEnabled" /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\YourPhone" /v "SmsEnabled" /t REG_DWORD /d 0 /f
+Get-StartApps | Where-Object { $_.Name -like "*messages*" }  # Find app name
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\<AppName>" -Name "Enabled" -Value 0 -PropertyType DWord -Force
+New-NetFirewallRule -DisplayName "Block SMS Sync" -Direction Outbound -Action Block -Program "%SystemRoot%\System32\YourPhone.exe"
+Get-AppxPackage *messages* | Remove-AppxPackage
+
