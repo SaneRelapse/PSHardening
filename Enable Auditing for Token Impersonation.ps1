7afb2fad7027fa1c84c3477a64bb4c86c9fc170b
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-02 05:58:57 -0900

    Add files via upload

---
+++ Enable Auditing for Token Impersonation.ps1
@@ -0,0 +1,3 @@
+﻿# Detecting Token Impersonation in Windows
+# Token impersonation allows attackers to execute code with higher privileges by "borrowing" another user’s security token.
+auditpol /set /subcategory:"Sensitive Privilege Use" /success:enable /failure:enable
