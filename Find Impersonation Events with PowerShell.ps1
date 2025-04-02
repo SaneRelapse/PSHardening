40d2a5aa7ebcd41a92714036cb3571e0732cc30d
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-02 05:57:25 -0900

    Add files via upload

---
+++ Find Impersonation Events with PowerShell.ps1
@@ -0,0 +1,8 @@
+﻿# Once auditing is enabled, look for these events in Event Viewer:
+
+# Event ID 4673 – A privileged service was called (SeImpersonatePrivilege used)
+
+# Event ID 4688 – A new process has been created (look for impersonation-related processes)
+
+# Event ID 4696 – A token was assigned to a process (possible impersonation)
+Get-WinEvent -LogName Security | Where-Object { $_.Id -eq 4673 -or $_.Id -eq 4696 } | Format-List
