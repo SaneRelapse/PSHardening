f0ed083a3af1d48e0e88556f5b996a6ce84e9439
SaneRelapse <jayden.crews@hotmail.ca>
2025-04-15 10:57:34 -0600

    Add files via upload

---
+++ Stop Shared Access.ps1
@@ -0,0 +1,5 @@
+﻿# Stop the SharedAccess service immediately
+Stop-Service -Name SharedAccess -Force
+
+# Prevent the service from starting at boot by setting its startup type to Disabled
+Set-Service -Name SharedAccess -StartupType Disabled
