b7925646ac62d98e5739effaa18f6cc4c814aff6
SaneRelapse <jayden.crews@hotmail.ca>
2025-05-10 01:24:20 -0600

    Update Hardening AI CoPilot.ps1

--- Hardening AI CoPilot.ps1
+++ Hardening AI CoPilot.ps1
@@ -42,3 +42,4 @@ Set-ProcessMitigation -Name copilot.exe -Enable EnableRopStackPivot
 Set-ProcessMitigation -Name copilot.exe -Enable EnableRopCallerCheck
 
 Write-Host "Mitigation policy fully applied to copilot.exe." -ForegroundColor Green
+# DO NOT ENABLE BlockDynamicCode IT WILL BREAK COPILOT
