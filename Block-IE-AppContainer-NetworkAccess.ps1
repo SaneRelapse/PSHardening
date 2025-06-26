44cc03cf7f131c150989f7fea7a86c79e4cd4a35
SaneRelapse <jayden.crews@hotmail.ca>
2025-06-25 21:18:47 -0600

    Add files via upload

---
+++ Block-IE-AppContainer-NetworkAccess.ps1
@@ -0,0 +1,8 @@
+﻿# Block all existing firewall rules tied to the Internet Explorer AppContainer (windows_ie_ac_001).
+# This hardens the system by:
+# - Preventing sandboxed IE (Enhanced Protected Mode) processes from accessing the network.
+# - Closing off legacy vectors that could be exploited by malicious ActiveX, COM, or legacy web content.
+# - Eliminating unnecessary AppContainer traffic from a deprecated browser (Internet Explorer).
+# - Reducing attack surface in systems that don't explicitly require legacy IE functionality.
+
+Get-NetFirewallRule -AppContainerName "windows_ie_ac_001" | Set-NetFirewallRule -Action Block
