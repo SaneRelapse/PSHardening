0565bb85e3097e4be38bae750cd3efa16381e680
SaneRelapse <jayden.crews@hotmail.ca>
2026-07-19 07:09:16 -0600

    Add files via upload

---
+++ CMD Banner via Registry.ps1
@@ -0,0 +1,7 @@
+﻿$cmdKey = "HKCU:\Software\Microsoft\Command Processor"
+
+if (-not (Test-Path $cmdKey)) {
+    New-Item -Path $cmdKey -Force | Out-Null
+}
+
+Set-ItemProperty -Path $cmdKey -Name "AutoRun" -Value 'echo YOU HAVE BEEN CAUGHT. DO NOT ATTEMPT TO ACCESS FILES, INJECT CODE, ESCALATE PRIVILEGES, OR ATTEMPT LATERAL MOVEMENT. CEASE AND DESIST IMMEDIATELY.'
